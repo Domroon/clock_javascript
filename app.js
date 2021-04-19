76e7416a01b38668daebd17afff0af39d79d2b7e
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-19 05:24:12 +0200

    update the time every second

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-let time = new Date();
-let formattedTime = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
-let timeContent = document.querySelector(".time-content")
-timeContent.innerText = formattedTime;
+var showCurrentTime = function (){
+
+    let time = new Date();
+    let timeContent = document.querySelector(".time-content")
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    timeContent.innerText = hours + ":" + minutes + ":" + seconds;
+}
+
+var oneSecond = 1000;
+setInterval(showCurrentTime, oneSecond);
