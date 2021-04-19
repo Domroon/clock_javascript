632677d8630ba516d1ac31822b00b36bf3c5b3f0
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-19 06:05:56 +0200

    add getters and setters

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+//Selectors
+const wakeupButton = document.querySelector('.wakeup_button');
+const lunchButton = document.querySelector('.lunch_button');
+const napButton = document.querySelector('.nap_button');
+
+//Event Listener
+wakeupButton.addEventListener('click', setWakeup);
+
+
+//Functions
 var showCurrentTime = function (){
 
-    let time = new Date();
-    let timeContent = document.querySelector(".time-content")
+    var time = new Date();
+    var timeContent = document.querySelector(".time-content")
 
     var hours = time.getHours();
     var minutes = time.getMinutes();
@@ -22,5 +32,38 @@ var showCurrentTime = function (){
     timeContent.innerText = hours + ":" + minutes + ":" + seconds;
 }
 
+function setWakeup (){
+    var wakeupTime = document.querySelector('.wakeup_time').value;
+}
+
+function getWakeup (){
+    var wakeupTime = document.querySelector('.wakeup_time').value;
+    return wakeupTime;
+}
+
+function getLunch (){
+    var lunchTime = document.querySelector('.lunch_time').value;
+    return lunchTime;
+}
+
+function getNap (){
+    var napTime = document.querySelector('.nap_time').value;
+    return napTime;
+}
+
+var setPicture = function(){
+    var time = new Date();
+    var formattedTime = time.getHours() + ":" + time.getMinutes();
+    if(getWakeup() == formattedTime){
+        console.log("YES!!");
+    }
+}
+
+//Execution
 var oneSecond = 1000;
+var oneMinute = 60000;
 setInterval(showCurrentTime, oneSecond);
+setInterval(setPicture, oneSecond);
+
+
+
